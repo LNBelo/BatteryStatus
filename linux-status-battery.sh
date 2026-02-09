78b737275aa4df213efd02a10cbdc589c097e9c7
LNBelo <lucasnascimentobelo@gmail.com>
2026-02-08 21:08:58 -0300

    linux update

--- linux-status-battery.sh
+++ linux-status-battery.sh
@@ -8,21 +8,25 @@ while true; do
     iPercent=$(cat /sys/class/power_supply/BAT0/capacity)
     sStatus=$(cat /sys/class/power_supply/BAT0/status)
 
-    # Verifica se está carregando (Charging)
     if [ "$sStatus" = "Charging" ]; then
         bCharging=true
     else
         bCharging=false
     fi
 
-    # Lógica de aviso: > 90% carregando ou < 30% descarregando
-    if [ "$bCharging" = true ] && [ "$iPercent" -gt 60 ]; then
+    # Lógica de aviso: Bateria acima de 90% (Carregando)
+    if [ "$bCharging" = true ] && [ "$iPercent" -gt 43 ]; then
         notify-send -u critical "Informação da Bateria" "A Bateria atingiu $iPercent%. Remova o carregador!" -i battery-full
+        # Toca um som de notificação (o '&' permite que o script continue sem esperar o som acabar)
+        paplay /usr/share/sounds/mint/stereo/notification-softer.ogg &
     fi
 
+    # Lógica de aviso: Bateria abaixo de 30% (Descarregando)
     if [ "$bCharging" = false ] && [ "$iPercent" -lt 30 ]; then
         notify-send -u critical "Informação da Bateria" "A Bateria atingiu $iPercent%. Conecte o carregador!" -i battery-caution
+        # Toca um som de alerta mais forte
+        paplay /usr/share/sounds/mint/stereo/notification-softer.ogg &
     fi
 
-    sleep 300 # 5 minutes
+    sleep 10000 #300 # 5 minutos
 done
